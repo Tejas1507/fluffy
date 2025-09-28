e92bad226f452711dcf260875666881ae14dffb7
tejas1507 <bemechtejas@gmail.com>
2025-09-28 16:23:23 +0530

    Finalize homepage design and add animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from './components/hero.jsx'
 import About from './components/About.jsx'
 import Navbar from './components/Navbar.jsx'
+import Features from './components/Features.jsx'
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-600'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Navbar/>
       <Hero />
       <About/>
+      <Features/>
     </main>
   )
 }
